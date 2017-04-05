ca63406d66cce5f7dd7722de235aef9a10b51f38
Laura Goldstein <laura.koss.goldstein@gmail.com>
2017-04-05 13:14:53 -0400

    autocomplete added to where in form

--- app.js
+++ app.js
@@ -103,8 +103,12 @@ function TripIndexControllerFunction($scope, TripFactory, $firebaseArray){
       console.log(map)
       map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -34.397, lng: 150.644},
-        zoom: 8
+        zoom: 2
+
       });
+      var input = document.getElementById('pac-input');
+      var autocomplete = new google.maps.places.Autocomplete(input);
+      
   }
   initMap();
 }
