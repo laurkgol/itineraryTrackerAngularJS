f83eeeed6bdef4fa960fcf9d3d0443d3a3426e9d
Laura Goldstein <laura.koss.goldstein@gmail.com>
2017-04-05 09:00:52 -0400

    trips and edit trips show

--- app.js
+++ app.js
@@ -11,17 +11,39 @@ angular
   "$firebaseArray",
   TripFactoryFunction
 ])
+// .factory("ActivityFactory", [
+//   "$firebaseObject",
+//   ActivityFactoryFunction
+// ])
 .controller("TripIndexController", [
   "$scope",
 "TripFactory",
   TripIndexControllerFunction
 ])
-
+// .controller("ActivityIndexController", [
+//   "$scope",
+//   "$firebaseArray",
+//   "ActivityFactory",
+//   ActivityIndexControllerFunction
+// ])
+.controller("TripShowController", [
+  "$scope",
+  "TripFactory",
+  "$stateParams",
+  TripShowControllerFunction
+])
 function TripFactoryFunction($firebaseArray){
   var ref = firebase.database().ref();
   return $firebaseArray(ref);
   }
 
+// function ActivityFactoryFunction($firebaseObject){
+//   return function(title){
+//     var ref = firebase.database().ref("activity").push();
+//     var activityTitleRef = ref.child(title)
+//     return $firebaseObject(activityTitleRef)
+//   }
+// }
 
 function RouterFunction($stateProvider){
   $stateProvider
@@ -31,6 +53,13 @@ function RouterFunction($stateProvider){
     controller: "TripIndexController",
     controllerAs: "vm"
   })
+  .state("TripShow", {
+    url: "/trips/:id",
+    templateUrl: "js/ng-views/trips/show.html",
+    controller: "TripShowController",
+    controllerAs: "vm"
+  })
+
 }
 
 function TripIndexControllerFunction($scope, TripFactory, $firebaseArray){
@@ -46,4 +75,29 @@ function TripIndexControllerFunction($scope, TripFactory, $firebaseArray){
     $scope.time_span = "",
     $scope.photo_url = ""
   }
+
+  // $scope.addActivity = function(){
+  //     ref = new Firebase("https://console.firebase.google.com/project/itinerarytracker/database/data/0/activity")
+  //
+  //     console.log(ref)
+  //     $scope.trips[0].$add({
+  //     date: $scope.date,
+  //     description: $scope.description,
+  //     photo_url: $scope.photo_url,
+  //     title: $scope.title
+  //   });
+  //   $scope.date ="",
+  //   $scope.description = "",
+  //   $scope.photo_url = "",
+  //   $scope.title = ""
+  // }
+}
+
+// function ActivityIndexControllerFunction($scope, ActivityFactory){
+//   ActivityFactory("Robot Restaurant").$bindTo($scope, "trip")
+// }
+
+function TripShowControllerFunction($scope, TripFactory, $stateParams){
+  console.log($scope)
+  $scope.trip = TripFactory.$stateParams
 }
